ee1fedc52de5015709c27e8d76fc3b6eff732896
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:43:19 +0200

    Update Terminal.java

--- Terminal.java
+++ Terminal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package terminal;
 
 import java.io.BufferedReader;
@@ -21,10 +17,6 @@ import java.util.Date;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-/**
- *
- * @author lenovo
- */
 public class Terminal {
 
     /**
