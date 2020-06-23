6831c943bac1a03e17d45c1ce5bc0514ff112459
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:42:47 +0200

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package terminal;
 
 import java.util.Vector;
 
-/**
- *
- * @author lenovo
- */
+
 public class Parser {
     String[] args; // Will be filled by arguments extracted by parse method  
     String  cmd; // Will be filled by the command extracted by parse method 
